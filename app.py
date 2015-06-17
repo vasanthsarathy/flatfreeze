dc2ba0e2ed0e7dda25c267d32c0b9850fbe835af
Vasanth Sarathy <vsarathy@gmail.com>
2015-06-17 10:19:56 -0400

    working flask app

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 #config
-app.config['FREEZER DESTINATION'] = 'gh-pages'
-app.config['FREEZER DESTINATION_IGNORE'] = ['.git*', 'CNAME',
-'.gitignore', 'readme.md']
+app.config['FREEZER_DESTINATION'] = '/gh-pages'
+app.config['FREEZER_DESTINATION_IGNORE'] = ['.git*', 'CNAME', '.gitignore', 'readme.md']
 app.config['FREEZER_RELATIVE_URLS'] = True
 
 #controllers

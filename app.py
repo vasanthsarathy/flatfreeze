1392e76fbdb41527f81b50c05c408016b0d16677
Vasanth Sarathy <vsarathy@gmail.com>
2015-06-17 09:36:39 -0400

    working Flask app

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+#initialization
+app = Flask(__name__)
+
+#config
+app.config['FREEZER DESTINATION'] = 'gh-pages'
+app.config['FREEZER DESTINATION_IGNORE'] = ['.git*', 'CNAME',
+'.gitignore', 'readme.md']
+app.config['FREEZER_RELATIVE_URLS'] = True
+
+#controllers
+@app.route("/")
+def index():
+	return render_template('index.html')
+
+#launch
+if __name__ == "__main__":
+	app.run(debug=True)
